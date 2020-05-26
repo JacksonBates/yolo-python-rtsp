a693bbe4ca80308796580ee9720759bdbf69f1c9
Jackson Bates <jacksonbates@hotmail.com>
2020-05-26 16:50:03 +1000

    some additions to get it running on my system

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -12,36 +12,73 @@ import numpy as np
 import imageio_ffmpeg as imageio
 import datetime
 
+# Addition needed to fix rtsp stream from my camera
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;0"
+
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--input', required=False,
-                help = 'path to input image', default = 'sampledata')
-ap.add_argument('-o', '--outputfile', required=False,
-                help = 'filename for output video', default='output.mp4')
-ap.add_argument('-od', '--outputdir', required=False,
-                help = 'path to output folder', default = 'output')
-ap.add_argument('-fs', '--framestart', required=False,
-                help = 'start frame', default=0)
-ap.add_argument('-fl', '--framelimit', required=False,
-                help = 'number of frames to process (0 = all)', default=0)
-ap.add_argument('-c', '--config', required=False,
-                help = 'path to yolo config file', default = 'cfg/yolov3-tiny.cfg')
-ap.add_argument('-w', '--weights', required=False,
-                help = 'path to yolo pre-trained weights', default = 'yolov3-tiny.weights')
-ap.add_argument('-cl', '--classes', required=False,
-                help = 'path to text file containing class names',  default = 'cfg/yolov3.txt')
-ap.add_argument('-ic', '--invertcolor', required=False,
-                help = 'invert RGB 2 BGR',  default = 'False')
-ap.add_argument('-fpt', '--fpsthrottle', required=False,
-                help = 'skips (int) x frames in order to catch up with the stream for slow machines 1 = no throttle',  default = 1)
+ap.add_argument(
+    "-i", "--input", required=False, help="path to input image", default="sampledata"
+)
+ap.add_argument(
+    "-o",
+    "--outputfile",
+    required=False,
+    help="filename for output video",
+    default="output.mp4",
+)
+ap.add_argument(
+    "-od", "--outputdir", required=False, help="path to output folder", default="output"
+)
+ap.add_argument("-fs", "--framestart", required=False, help="start frame", default=0)
+ap.add_argument(
+    "-fl",
+    "--framelimit",
+    required=False,
+    help="number of frames to process (0 = all)",
+    default=0,
+)
+ap.add_argument(
+    "-c",
+    "--config",
+    required=False,
+    help="path to yolo config file",
+    default="cfg/yolov3-tiny.cfg",
+)
+ap.add_argument(
+    "-w",
+    "--weights",
+    required=False,
+    help="path to yolo pre-trained weights",
+    default="yolov3-tiny.weights",
+)
+ap.add_argument(
+    "-cl",
+    "--classes",
+    required=False,
+    help="path to text file containing class names",
+    default="cfg/yolov3.txt",
+)
+ap.add_argument(
+    "-ic", "--invertcolor", required=False, help="invert RGB 2 BGR", default="False"
+)
+ap.add_argument(
+    "-fpt",
+    "--fpsthrottle",
+    required=False,
+    help="skips (int) x frames in order to catch up with the stream for slow machines 1 = no throttle",
+    default=1,
+)
 args = ap.parse_args()
 
+
 def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 
 def get_output_layers(net):
 
@@ -51,26 +88,38 @@ def get_output_layers(net):
 
     return output_layers
 
+
 def save_bounded_image(image, class_id, confidence, x, y, x_plus_w, y_plus_h):
     label = str(classes[class_id])
-    dirname = os.path.join(args.outputdir, label, datetime.datetime.now().strftime('%Y-%m-%d'))
+    dirname = os.path.join(
+        args.outputdir, label, datetime.datetime.now().strftime("%Y-%m-%d")
+    )
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
-    filename = label + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S_%f') + '_conf' + "{:.2f}".format(confidence) + '.jpg'
-    print ('Saving bounding box:' + filename)
+    filename = (
+        label
+        + "_"
+        + datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S_%f")
+        + "_conf"
+        + "{:.2f}".format(confidence)
+        + ".jpg"
+    )
+    print("Saving bounding box:" + filename)
     roi = image[y:y_plus_h, x:x_plus_w]
     if roi.any():
         if str2bool(args.invertcolor) == False:
             roi = cv2.cvtColor(roi, cv2.COLOR_RGB2BGR)
         cv2.imwrite(os.path.join(dirname, filename), roi)
 
+
 def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     label = str(classes[class_id])
     color = COLORS[class_id]
 
-    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 3)
-    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 3)
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 3)
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 3)
+
 
 def detect(image):
 
@@ -80,7 +129,7 @@ def detect(image):
 
     net = cv2.dnn.readNet(args.weights, args.config)
 
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
 
     net.setInput(blob)
 
@@ -108,7 +157,6 @@ def detect(image):
                 confidences.append(float(confidence))
                 boxes.append([x, y, w, h])
 
-
     indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
     orgImage = image.copy()
@@ -119,11 +167,28 @@ def detect(image):
         y = box[1]
         w = box[2]
         h = box[3]
-        save_bounded_image(orgImage, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
-        draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+        save_bounded_image(
+            orgImage,
+            class_ids[i],
+            confidences[i],
+            round(x),
+            round(y),
+            round(x + w),
+            round(y + h),
+        )
+        draw_prediction(
+            image,
+            class_ids[i],
+            confidences[i],
+            round(x),
+            round(y),
+            round(x + w),
+            round(y + h),
+        )
     if str2bool(args.invertcolor) == True:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image
+
 
 def processvideo(file):
     cap = cv2.VideoCapture(file)
@@ -131,58 +196,74 @@ def processvideo(file):
     writer = imageio.write_frames(args.outputfile, (int(cap.get(3)), int(cap.get(4))))
     writer.send(None)
     frame_counter = 0
-    while(cap.isOpened()):
+    while cap.isOpened():
         frame_counter = frame_counter + 1
         ret, frame = cap.read()
-        print('Detecting objects in frame ' + str(frame_counter))
-        if ret==True:
+        print("Detecting objects in frame " + str(frame_counter))
+        if ret == True:
             if not frame is None:
                 image = detect(frame)
                 writer.send(frame)
             else:
-                print('Frame error in frame ' + str(frame_counter))
+                print("Frame error in frame " + str(frame_counter))
         else:
             break
     cap.release()
     writer.close()
 
-# Doing some Object Detection on a video
-classes = None
 
-with open(args.classes, 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-if args.input.startswith('rtsp'):
-
-    cap = cv2.VideoCapture(args.input)
+def handle_stream(cap):
     if int(args.framelimit) > 0:
-        writer = imageio.write_frames(args.outputfile, (int(cap.get(3)), int(cap.get(4))))
+        writer = imageio.write_frames(
+            args.outputfile, (int(cap.get(3)), int(cap.get(4)))
+        )
         writer.send(None)
     frame_counter = 0
-    while(True):
-        if int(args.framelimit) > 0 and frame_counter > int(args.framestart) + int(args.framelimit):
+    while True:
+        if int(args.framelimit) > 0 and frame_counter > int(args.framestart) + int(
+            args.framelimit
+        ):
             writer.close()
             break
 
-        if frame_counter % int(args.fpsthrottle) ==0:
+        if frame_counter % int(args.fpsthrottle) == 0:
+            ret, frame = cap.read()
+
             ret, frame = cap.read()
             if ret and frame_counter >= int(args.framestart):
-                print('Detecting objects in frame ' + str(frame_counter))
+                print("Detecting objects in frame " + str(frame_counter))
                 frame = detect(frame)
                 if int(args.framelimit) > 0:
                     writer.send(frame)
             else:
-                print('Skipping frame ' + str(frame_counter))
+                print("Skipping frame " + str(frame_counter))
         else:
-            print('FPS throttling. Skipping frame ' + str(frame_counter))
-        frame_counter=frame_counter+1
+            print("FPS throttling. Skipping frame " + str(frame_counter))
+        frame_counter = frame_counter + 1
+
+
+# Doing some Object Detection on a video
+classes = None
+
+with open(args.classes, "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+if args.input.startswith("rtsp"):
+
+    cap = cv2.VideoCapture(args.input)
+    handle_stream(cap)
+
+elif args.input.startswith("webcam"):
+
+    cap = cv2.VideoCapture(0)
+    handle_stream(cap)
 
 else:
     if os.path.isdir(args.input):
         for dirpath, dirnames, filenames in os.walk(args.input):
             for filename in [f for f in filenames if f.endswith(".mp4")]:
-                print('Processing video ' + os.path.join(dirpath, filename))
+                print("Processing video " + os.path.join(dirpath, filename))
                 processvideo(os.path.join(dirpath, filename))
     else:
         processvideo(os.path.join(args.input))
